0e052f6479d1326048a04cd854fc3bf6fdb2230f
AhoaT <ahoa.tran@gmail.com>
2022-03-14 22:39:25 +0000

    added validation for checkboxes

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const user = localStorage.getItem("username");
 
 email.setAttribute("value", user);
 
+const emailCheckbox = document.querySelector("#email-check");
+const mobileCheckbox = document.querySelector("#mobile-check");
+const submitBtn = document.querySelector(".submit-btn");
+const error = document.querySelector(".error")
+const waitingList = document.querySelector("#waiting-list");
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (emailCheckbox.checked == false && mobileCheckbox.checked == false) {
+        error.style.visibility = "visible";
+    } else {
+        waitingList.submit();
+    }
+});
